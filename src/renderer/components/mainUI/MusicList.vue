88acd999944c269dc6125f696a08a87f3277d884
Shouta <bob1996w@gmail.com>
2018-03-01 02:07:21 +0800

    improve music list visual and interactivity

--- src/renderer/components/mainUI/MusicList.vue
+++ src/renderer/components/mainUI/MusicList.vue
@@ -1,8 +1,23 @@
 <template>
     <div class="music-list">
-        <ul>
-            <template v-for="music in musics">
-                <li>{{music.meta.title}}</li>
+        <ul class="list-group list-group-flush">
+            <template v-for="(music, index) in musics">
+                <div class="list-group-item list-group-item-action clearfix" @click="playMusicItem(index)">
+                    <span class="float-left mr-2">
+                        <i class="fas fa-music"></i>
+                    </span>
+                    <span>
+                        {{index + 1}}. {{music.meta.title}}
+                    </span>
+                    <span class="float-right">
+                        <span class="mr-2">
+                            {{getStringTime(music.meta.duration)}}
+                        </span>
+                        <button class="btn btn-sm btn-warning" @click.stop="removeMusicItem(index)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                    </span>
+                </div>
             </template>
         </ul>
     </div>
@@ -12,17 +27,35 @@
 export default {
     name: "music-list",
     mounted: function() {
-    // initialize all tooltips
-    this.$electron.ipcRenderer.on('get-musics',(event ,arg) => {
-      this.musics.push(arg);
-      console.log(arg)
-    })
-  },
+        // initialize all tooltips
+        this.$electron.ipcRenderer.on('get-musics',(event ,arg) => {
+            this.musics.push(arg);
+        })
+    },
     data: function() {
         return {
             musics: []
         };
     },
+    methods: {
+        getStringTime: function(seconds) {
+            var mins = ~~(seconds / 60);
+            var secs = ~~(seconds % 60);
+            return String( mins ) + "\'" + (secs < 10 ? "0" : "") + String( secs );
+        },
+        playMusicItem: function(index) {
+            /* TODO: 
+                call ipcMain to play music
+                broadcast music tags to PlayerControl (this.musics[index] object)
+            */
+            console.log("Play " + this.musics[index].path);
+
+        },
+        removeMusicItem: function(index) {
+            console.log("Remove " + this.musics[index].path);
+            this.musics.splice(index, 1);
+        }
+    }
 }
 </script>
 
